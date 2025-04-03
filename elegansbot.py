a3ac9ef36e6c3a918b213d7d5f81f1aef064e463
Federico Maggiore <federico.maggiore@tutamail.com>
2025-04-03 22:19:57 +0200

    moved plot_kymogram as static method

--- elegansbot.py
+++ elegansbot.py
@@ -152,28 +152,6 @@ def plot_outline_n_trajectory(
 
     return ax.lines + ax.texts
 
-# %%
-def plot_kymogram(K):
-    matrix = K.transpose()
-    shape = np.shape(matrix)
-
-    fig, ax = plt.subplots(dpi=120)
-    im = ax.imshow(matrix, aspect=.3 * (shape[1] / shape[0]), cmap='bwr')
-    ax.set_title('Kymogram')
-    ax.set_xlabel('frame number')
-    ax.set_ylabel('body position')
-    ax.set_yticks([0, shape[0] - 1])
-    ax.set_yticklabels(['head', 'tail'])
-
-    [l, b, w, h] = ax.get_position().bounds
-    cbound = [l + w * 1.05, b, w * 0.05, h]
-    cax = fig.add_axes(cbound)
-    cbar = fig.colorbar(im, ax=ax, cax=cax)
-    cax.set_title('dorsal', fontsize=8)
-    cax.set_xlabel('ventral', fontsize=8)
-
-    set_bbox_inches_tight(fig)
-    plt.show()
 
 # %%
 @njit(fastmath=True, cache=True) # enables all fast-math flags of llvm
@@ -1073,6 +1051,29 @@ class Worm:
         set_bbox_inches_tight(fig)
         plt.show()
         print(f"average worm speed: {np.mean(speed_log):.5f} (mm/sec)")
+
+    @staticmethod
+    def plot_kymogram(K):
+        matrix = K.transpose()
+        shape = np.shape(matrix)
+
+        fig, ax = plt.subplots(dpi=120)
+        im = ax.imshow(matrix, aspect=.3 * (shape[1] / shape[0]), cmap='bwr')
+        ax.set_title('Kymogram')
+        ax.set_xlabel('frame number')
+        ax.set_ylabel('body position')
+        ax.set_yticks([0, shape[0] - 1])
+        ax.set_yticklabels(['head', 'tail'])
+
+        [l, b, w, h] = ax.get_position().bounds
+        cbound = [l + w * 1.05, b, w * 0.05, h]
+        cax = fig.add_axes(cbound)
+        cbar = fig.colorbar(im, ax=ax, cax=cax)
+        cax.set_title('dorsal', fontsize=8)
+        cax.set_xlabel('ventral', fontsize=8)
+
+        set_bbox_inches_tight(fig)
+        plt.show()
 
     def save(self, file_name):
         """
