69831e4b177524d6583fb2d9818384a91ea87442
Maggiore93 <federico.maggiore@tutamail.com>
2025-05-21 14:44:06 +0200

    added setup.py in order to install with pip and updated requirements without versions of single packages

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="elegansbot_conan",  # Your project name
+    version="0.1",            # Initial version
+    packages=find_packages(), # Automatically find all packages
+)
